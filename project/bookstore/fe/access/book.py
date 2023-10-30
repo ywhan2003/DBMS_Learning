c1b1b7ba081beb30d16c9d0a385a3d72396efe73
TakanashiXny <2110520134@qq.com>
2023-10-30 11:34:21 +0800

    fe.book

--- project/bookstore/fe/access/book.py
+++ project/bookstore/fe/access/book.py
@@ -3,6 +3,8 @@ import sqlite3 as sqlite
 import random
 import base64
 import simplejson as json
+import pymongo
+from be.model import store
 
 
 class Book:
@@ -30,33 +32,43 @@ class Book:
 
 class BookDB:
     def __init__(self, large: bool = False):
-        parent_path = os.path.dirname(os.path.dirname(__file__))
-        self.db_s = os.path.join(parent_path, "data/book.db")
-        self.db_l = os.path.join(parent_path, "data/book_lx.db")
-        if large:
-            self.book_db = self.db_l
-        else:
-            self.book_db = self.db_s
+        # parent_path = os.path.dirname(os.path.dirname(__file__))
+        # self.db_s = os.path.join(parent_path, "data/book.db")
+        # self.db_l = os.path.join(parent_path, "data/book_lx.db")
+        # if large:
+        #     self.book_db = self.db_l
+        # else:
+        #     self.book_db = self.db_s
+        self.host = '127.0.0.1'
+        self.port = 27017
+        self.client = store.get_db_client()
+        self.db = self.client.bookstore
 
     def get_book_count(self):
-        conn = sqlite.connect(self.book_db)
-        cursor = conn.execute("SELECT count(id) FROM book")
-        row = cursor.fetchone()
-        return row[0]
+        # conn = sqlite.connect(self.book_db)
+        # cursor = conn.execute("SELECT count(id) FROM book")
+        # row = cursor.fetchone()
+        # return row[0]
+        users_col = self.db.books
+        result = users_col.count_documents()
+        return result
+
 
     def get_book_info(self, start, size) -> [Book]:
         books = []
-        conn = sqlite.connect(self.book_db)
-        cursor = conn.execute(
-            "SELECT id, title, author, "
-            "publisher, original_title, "
-            "translator, pub_year, pages, "
-            "price, currency_unit, binding, "
-            "isbn, author_intro, book_intro, "
-            "content, tags, picture FROM book ORDER BY id "
-            "LIMIT ? OFFSET ?",
-            (size, start),
-        )
+        # conn = sqlite.connect(self.book_db)
+        # cursor = conn.execute(
+        #     "SELECT id, title, author, "
+        #     "publisher, original_title, "
+        #     "translator, pub_year, pages, "
+        #     "price, currency_unit, binding, "
+        #     "isbn, author_intro, book_intro, "
+        #     "content, tags, picture FROM book ORDER BY id "
+        #     "LIMIT ? OFFSET ?",
+        #     (size, start),
+        # )
+        users_col = self.db.books
+        cursor = users_col.find().sort("book_id", pymongo.ASCENDING).skip(start).limit(size)
         for row in cursor:
             book = Book()
             book.id = row[0]
