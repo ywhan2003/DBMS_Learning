a9e9a7f3e5bc68040d2a412a3b74f421e1e1e855
Ryan_Watson <1136021998@qq.com>
2023-10-30 20:43:07 +0800

    Update book.py

--- project/bookstore/build/lib/fe/access/book.py
+++ project/bookstore/build/lib/fe/access/book.py
@@ -3,6 +3,8 @@ import sqlite3 as sqlite
 import random
 import base64
 import simplejson as json
+import pymongo
+from be.model import store
 
 
 class Book:
@@ -30,54 +32,64 @@ class Book:
 
 class BookDB:
     def __init__(self, large: bool = False):
-        parent_path = os.path.dirname(os.path.dirname(__file__))
-        self.db_s = os.path.join(parent_path, "data/book.db")
-        self.db_l = os.path.join(parent_path, "data/book_lx.db")
-        if large:
-            self.book_db = self.db_l
-        else:
-            self.book_db = self.db_s
+        # parent_path = os.path.dirname(os.path.dirname(__file__))
+        # self.db_s = os.path.join(parent_path, "data/book.db")
+        # self.db_l = os.path.join(parent_path, "data/book_lx.db")
+        # if large:
+        #     self.book_db = self.db_l
+        # else:
+        #     self.book_db = self.db_s
+        self.host = '127.0.0.1'
+        self.port = 27017
+        self.client = store.get_db_client()
+        self.db = self.client.bookstore
 
     def get_book_count(self):
-        conn = sqlite.connect(self.book_db)
-        cursor = conn.execute("SELECT count(id) FROM book")
-        row = cursor.fetchone()
-        return row[0]
+        # conn = sqlite.connect(self.book_db)
+        # cursor = conn.execute("SELECT count(id) FROM book")
+        # row = cursor.fetchone()
+        # return row[0]
+        users_col = self.db.books
+        result = users_col.count_documents()
+        return result
+
 
     def get_book_info(self, start, size) -> [Book]:
         books = []
-        conn = sqlite.connect(self.book_db)
-        cursor = conn.execute(
-            "SELECT id, title, author, "
-            "publisher, original_title, "
-            "translator, pub_year, pages, "
-            "price, currency_unit, binding, "
-            "isbn, author_intro, book_intro, "
-            "content, tags, picture FROM book ORDER BY id "
-            "LIMIT ? OFFSET ?",
-            (size, start),
-        )
+        # conn = sqlite.connect(self.book_db)
+        # cursor = conn.execute(
+        #     "SELECT id, title, author, "
+        #     "publisher, original_title, "
+        #     "translator, pub_year, pages, "
+        #     "price, currency_unit, binding, "
+        #     "isbn, author_intro, book_intro, "
+        #     "content, tags, picture FROM book ORDER BY id "
+        #     "LIMIT ? OFFSET ?",
+        #     (size, start),
+        # )
+        users_col = self.db.books
+        cursor = users_col.find().sort("book_id", pymongo.ASCENDING).skip(start).limit(size)
         for row in cursor:
             book = Book()
-            book.id = row[0]
-            book.title = row[1]
-            book.author = row[2]
-            book.publisher = row[3]
-            book.original_title = row[4]
-            book.translator = row[5]
-            book.pub_year = row[6]
-            book.pages = row[7]
-            book.price = row[8]
+            book.id = row["book_id"]
+            book.title = row["title"]
+            book.author = row["author"]
+            book.publisher = row["publisher"]
+            book.original_title = row["original_title"]
+            book.translator = row["translator"]
+            book.pub_year = row["pub_year"]
+            book.pages = row["pages"]
+            book.price = row["price"]
 
-            book.currency_unit = row[9]
-            book.binding = row[10]
-            book.isbn = row[11]
-            book.author_intro = row[12]
-            book.book_intro = row[13]
-            book.content = row[14]
-            tags = row[15]
+            book.currency_unit = row["current_unit"]
+            book.binding = row["binding"]
+            book.isbn = row["isbn"]
+            book.author_intro = row["author_intro"]
+            book.book_intro = row["book_intro"]
+            book.content = row["content"]
+            tags = row["tags"]
 
-            picture = row[16]
+            picture = row["picture"]
 
             for tag in tags.split("\n"):
                 if tag.strip() != "":
